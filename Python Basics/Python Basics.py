aa70de2ddf776f5f10adc02d5ef82619137d8545
Harry <asad@code.com>
2020-04-24 01:37:08 +0500

    Old files were delted. Python  Basic operat files were added

---
+++ Python Basics/Python Basics.py
@@ -0,0 +1,17 @@
+# This is all example of strings and print function in python
+print("This is my First code in python")
+print("This is a string")
+print('This is a single qoute')
+print("This is double qoute")
+print("This is 'single qoute' inside double qoute")
+print('This is "double qoute" inside single qoute')
+#++++++++++++Escape Sequnce in Python++++++++++++
+print("This is First line\nThis is Second line")
+print("This is example of\t tab\nThis is example of \t\tdouble tab")
+print("This is example of single slash\\ ")
+print("This is example of\\\\double slash")
+print("Adding extra slash \\n neutralize the effect ")
+print("\"This is example of \"double qoutes\" inside double qoute\"")
+print('\'This is example of \'double qoute\' inside double qoutes\'')
+print('This is\bbackspace')
+# jsdlkfjsd lfj;sd ctrl+/ will make whole line comment or multiple lines to commen
---
+++ Python Basics/String Operation.py
@@ -0,0 +1,13 @@
+
+#Concatenation of strings
+fname="Salman"
+lname="Qamar "
+Full_name =  fname+lname
+full_name =  fname+" "+lname  # Also concatinate space
+print(Full_name)
+print(full_name)
+# Multiply string
+print(full_name*4)
+print("Hello World" + " 1") # Everything inside qoutes is string. Cannot add string with digit
+# print("Hello World"+ 1) Error
+
