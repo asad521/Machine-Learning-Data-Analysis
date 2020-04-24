fede8d99c1ecd35ac088cf5cdfa66e8a41fe43dc
Harry <asad@code.com>
2020-04-25 01:30:21 +0500

    Add  list,tuple and  update  old file

---
+++ Python Course/practice.py
@@ -0,0 +1,23 @@
+# total=0
+# for i in range(5):
+#     total=total+i
+#     print(f"Value of i is {i} and total is {total}")
+# in keyword is special
+# When used with condition(if), It can use it to find any character or word in any  long string/integer/list.
+# # When you use in with loop, then it becomes index, they run from lenght of range function
+# # for i in (10): # i will run from 0 to 9
+# # for i in range(m,n,p)
+#     m is start index (integer), n is end index number (integer), p is step
+# for i in range(variable)
+#     In variable ,you can write any variable,string, list.Loop index will according to length of variable
+#
+#
+#
+#
+# name2="asadali"
+# if "i" in name2:
+#     print("asad")
+# l=[1,2,"asad",2.5,None]
+# print(l)
+# l[:]="123 4"
+# print(l)
